da50f3d2e48520149ec7b4a2bf8b6f4229f1646f
Lukashin Andrey <loukash@gmail.com>
2015-10-03 16:47:26 +0500

    Работа с базой через расширение FlaskDB

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 DEBUG = False
 SECRET_KEY = '\xe7\x16\x06\xc7\xe8\xc7\xae\xb9\x94y\x0f\xbc\x06t{\xd0\xa2\xce/\xf8H~7\x0e'
-DATABASE_URI = 'sqlite:///db.sqlite3'
-# DATABASE_URI = 'sqlite:///my_database'
-# DATABASE_URI = 'mysql://user:password@ip:port/my_database'
-# DATABASE_URI = 'postgresql://user:password@ip:port/my_database'
+DATABASE = 'sqlite:///db.sqlite3'
+# DATABASE = 'sqlite:///my_database'
+# DATABASE = 'mysql://user:password@ip:port/my_database'
+# DATABASE = 'postgresql://user:password@ip:port/my_database'
 AUTH_URL = '/auth'
 LOGIN_URL = '/login'
 COOKIE_NAME = 'x-factor-sid'
